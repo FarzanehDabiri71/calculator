6194167054afe174c01d85ce4a0f00fb826cad7d
unknown <farzanehdabiri1371@yahoo.com>
2024-06-25 21:22:14 +0330

    active operators

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
+import { useState } from "react";
 import "./App.css";
-
+const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+const operators = ["÷", "x", "-", "+"];
 function App() {
-  const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
-  const operators = ["÷", "x", "-", "+"];
-  const handleClick = () => {};
-  const handleResult =()=>{}
+  const [display, setDisplay] = useState("");
+  const [expression, setExpression] = useState([]);
+  const handleClick = (value) => {
+    // setDisplay(value);
+    setExpression([...expression, value]);
+  };
+  const handleResult = () => {
+    const result = expression
+      .join("")
+      .split(/(\D)/g)
+      .map((value) => (value.match(/\d/g) ? parseInt(value, 0) : value))
+      .reduce((acc, value, index, array) => {
+        switch (value) {
+          case "+":
+            return (acc = acc + array[index + 1]);
+          case "-":
+            return (acc = acc - array[index + 1]);
+          case "*":
+            return (acc = acc * array[index + 1]);
+          case "/":
+            return (acc = acc / array[index + 1]);
+          default:
+            return acc;
+        }
+      });
+    setDisplay(result);
+    setExpression("");
+  };
+  const handleClear =()=>{
+    setDisplay("")
+    setExpression([])
+  }
   return (
     <div className="App">
-      <h3 className="display"></h3>
-      <span className="expression"></span>
+      <h3 className="display">{display}</h3>
+      <span className="expression">{expression}</span>
       <section className="panel">
         {/* numbers */}
         <section className="numbers">
@@ -17,6 +47,7 @@ function App() {
               {number}
             </button>
           ))}
+          <button onClick={handleClear}>C</button>
         </section>
         {/* operators */}
         <section className="operators">
